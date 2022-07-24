464b3823e65185c4282d361a07bad2ae81888697
mohammadpmf <mohammad.pfallah@gmail.com>
2022-07-24 19:01:54 +0430

    2 Mordad

--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def show_blog(request):
-    return render(request, 'blog/index.html')
+    context = {'posts': Post.objects.all()}
+    return render(request, 'blog/index.html', context)
