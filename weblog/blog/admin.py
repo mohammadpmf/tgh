d3c2e69d631b92e2b3da9605c7f2513f50da46b5
mohammadpmf <mohammad.pfallah@gmail.com>
2022-08-09 19:05:14 +0430

    18 mordad

--- weblog/blog/admin.py
+++ weblog/blog/admin.py
@@ -1,4 +1,3 @@
-from ast import Pass
 from django.contrib import admin
 
 from .models import Post
--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Post
 
 def show_blog(request):
-    context = {'posts': Post.objects.all()}
+    # context = {'posts': Post.objects.all()}
+    context = {'posts': Post.objects.filter(status='pub')}#:]
     return render(request, 'blog/index.html', context)
 
 def show_detail(request, pk):
