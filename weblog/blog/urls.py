15f970dc5a75b39f3379f1e725f0e0f01e418165
mohammadpmf <mohammad.pfallah@gmail.com>
2022-08-14 19:06:04 +0430

    23 mordad

--- weblog/blog/urls.py
+++ weblog/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.show_blog, name='show_blog'),
     path('<int:pk>', views.show_detail, name='show_detail'),
+    path('new_post/', views.new_post, name='new_post'),
 ]
--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.shortcuts import render
 from .models import Post
 
@@ -6,6 +7,12 @@ def show_blog(request):
     context = {'posts': Post.objects.filter(status='pub')}#:]
     return render(request, 'blog/index.html', context)
 
+def new_post(request):
+    return render(request, 'blog/index.html')
+
+
 def show_detail(request, pk):
+    if request.method == "POST":
+        print("OK")
     context = {'post': Post.objects.get(pk=pk)}
     return render(request, 'blog/post_detail.html', context)
