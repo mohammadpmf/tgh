c0a3fc65d33af42155e0d9ea3773e29e22c22cb1
mohammadpmf <mohammad.pfallah@gmail.com>
2022-09-06 19:01:29 +0430

    15 shahrivar

--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -1,25 +1,32 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 
 def show_blog(request):
     # context = {'posts': Post.objects.all()}
-    context = {'posts': Post.objects.filter(status='pub')}#:]
+    context = {'posts': Post.objects.filter(status='pub').order_by('-date_modified')}
     return render(request, 'blog/index.html', context)
 
 def new_post(request):
+    from .forms import PostForm
     if request.method == "POST":
-        # from .forms import PostForm
-        # PostForm(request.POST)
-        t = request.POST.get('title')
-        te = request.POST.get('text')
-        from django.contrib.auth.models import User
-        Post.objects.create(title=t, text=te,
-                            author=User.objects.first(), status='pub')
-
+        post = PostForm(request.POST)
+        if post.is_valid():
+            post.save()
+            return redirect("blog")
     else:
-        print("Get")
-    return render(request, 'blog/new_post.html')
+        post = PostForm()
+    return render(request, 'blog/new_post.html', context={'post':post})
+
+        # t = request.POST.get('title')
+        # te = request.POST.get('text')
+        # from django.contrib.auth.models import User
+        # Post.objects.create(title=t, text=te,
+        #                     author=User.objects.first(), status='pub')
+
+    # else:
+    #     print("Get")
+    # return render(request, 'blog/new_post.html')
 
 
 def show_detail(request, pk):
